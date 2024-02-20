babd899a7612680bd7f99d59c090ad33e43172e2
lin <lin@aaa.com>
2024-02-20 08:56:12 +0000

    remix connect config

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 DIR_DATA="/wallet/ethereum/test"
-INTERVAL_MINE=13
+INTERVAL_MINE=3
 
 $DIR_DATA/stop.sh
 echo -e "Starting \n"
 
-geth --dev --dev.period $INTERVAL_MINE --config $DIR_DATA/config.toml --datadir $DIR_DATA >$DIR_DATA/stdout.txt 2>$DIR_DATA/stderr.txt &
+geth --vmdebug --dev --dev.period $INTERVAL_MINE --config $DIR_DATA/config.toml --datadir $DIR_DATA >$DIR_DATA/stdout.txt 2>$DIR_DATA/stderr.txt &
 #geth --dev --dev.period $INTERVAL_MINE --config $DIR_DATA/config.toml --datadir $DIR_DATA console
 echo $! >$DIR_DATA/node.pid
