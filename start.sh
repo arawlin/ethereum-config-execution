9c3f812cba4e75d5b63d7101fba86811abe4ef36
arawlin <arawlinlin@gmail.com>
2025-06-09 00:44:32 +0800

    update

--- start.sh
+++ start.sh
@@ -5,6 +5,6 @@ DIR_DATA="/wallet/ethereum/execution"
 $DIR_DATA/stop.sh
 echo -e "Starting \n"
 
-../reth/target/maxperf/reth node --config $DIR_DATA/config.toml --datadir $DIR_DATA --http --ws --auth-ipc --auth-ipc.path $DIR_DATA/reth_engine_api.ipc --authrpc.jwtsecret ../jwt.hex --log.file.directory $DIR_DATA/logs >$DIR_DATA/stdout.txt 2>$DIR_DATA/stderr.txt > /dev/null 2>&1 &
+../reth/target/maxperf/reth node --config $DIR_DATA/config.toml --datadir $DIR_DATA --http --ws --auth-ipc --auth-ipc.path $DIR_DATA/reth_engine_api.ipc --authrpc.jwtsecret ../jwt.hex --log.file.directory $DIR_DATA/logs > /dev/null 2>&1 &
 echo $! >$DIR_DATA/node.pid
 
